367e8f3ce6a479efd04a5359aedef3bf62ad6522
Nikita <niksipeykin@mail.ru>
2020-09-06 10:24:46 +0300

    Initial commit

--- lesson3/src/ru/geekbrains/lesson3/Main.java
+++ lesson3/src/ru/geekbrains/lesson3/Main.java
@@ -10,33 +10,45 @@ public class Main {
         загаданное, или меньше. После победы или проигрыша выводится запрос
         – «Повторить игру еще раз? 1 – да / 0 – нет»(1 – повторить, 0 – нет).*/
     public static void main(String[] args) {
-
         System.out.println("Ваша задача угадать число!");
         gameTry(makeANumber(),range());
-
     }
-    static boolean gameTry(int b,int c){
+    static void gameTry(int b,int c){
         int tryCount = 3; //колличество попыток
         while (true){
             System.out.println("Угадайте число от 0 до "+c);
             int a = playerNumber();
             if(a == b){
                 System.out.println("Вы угадали!");
-                return true;
             }
             else if(a > b){
                 tryCount -=1;
                 System.out.println("Загаданное число меньше :(\nУ вас осталось "+tryCount+" попытки");
+
                 if (tryCount == 0){
                     System.out.println("Вы проиграли!");
-                    return false;
+                }
+                else{
+                    System.out.println("Желаете продолжить?\n1 = да\n0 = нет");
+                    if(playerNumber()==1){
+                        System.out.println();
+                    }else
+                        System.out.println("игра окончена");
+                       break;
                 }
             }else {
                 tryCount -=1;
                 System.out.println("Загаданное число больше :(\nУ вас осталось "+tryCount+" попытки");
                 if (tryCount == 0){
                     System.out.println("Вы проиграли!");
-                    return false;
+                }
+                else{
+                    System.out.println("Желаете продолжить?\n1 = да\n0 = нет");
+                    if(playerNumber()==1){
+                        System.out.println();
+                    }else
+                        System.out.println("игра окончена");
+                        break;
                 }
             }
         }
