eb83377d80565a0ef65cc81660de29a5fa1bc21a
Nikita <Niksipeykin@mail.ru>
2020-09-11 00:29:34 +0300

    Task 3 completed

--- lesson5/src/ru/geekbrains/lesson5/StaffMember.java
+++ lesson5/src/ru/geekbrains/lesson5/StaffMember.java
@@ -1,7 +1,7 @@
 package ru.geekbrains.lesson5;
 
 public class StaffMember {
-   private String firstName;
+    private String firstName;
     private String lastName;
     private String patronymic;
     private String position;
@@ -12,12 +12,18 @@ public class StaffMember {
 
     public StaffMember(String firstName, String lastName, String patronymic, String position, String eMail,
                        String phoneNumber, int salary, int age){
-
-
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.patronymic = patronymic;
+        this.position = position;
+        this.eMail = eMail;
+        this.phoneNumber = phoneNumber;
+        this.salary = salary;
+        this.age = age;
+        printStaffMember();
     }
-    static void printStaffMember(){
+     void printStaffMember(){
         System.out.println("Имя "+firstName+"\nФамилия "+lastName+"\nОтчество "+patronymic+"\nДолжность "+position+
             "\neMail "+eMail+"\nТелефон "+phoneNumber+"\nЗарплата "+salary+"\nВозраст "+age);
-
     }
 }
