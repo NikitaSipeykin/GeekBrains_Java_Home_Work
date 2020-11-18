b17a25c7fd8cbbc55fa6072fd18b5ce9707a643c
Nikita <Niksipeykin@mail.ru>
2020-11-19 01:46:44 +0300

    Finish lesson2

--- lesson2/src/ru/geekbrains/lesson2/Main.java
+++ lesson2/src/ru/geekbrains/lesson2/Main.java
@@ -5,7 +5,7 @@ import java.util.Random;
 
 public class Main {
 
-    public static void main(String[] args) {
+
         /*1. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ].
          С помощью цикла и условия заменить 0 на 1, 1 на 0;
         2. Задать пустой целочисленный массив размером 8. С помощью цикла заполнить его значениями 0 3 6 9 12 15 18 21;
@@ -20,14 +20,11 @@ public class Main {
         7. **** Написать метод, которому на вход подается одномерный массив и число n (может быть положительным, или
         отрицательным), при этом метод должен сместить все элементымассива на n позиций. Для усложнения задачи нельзя
         пользоваться вспомогательными массивами.*/
-        task1();
-        task2();
-        task3();
-        task4();
-        task5();
 
-    }
+
     //first task______________________________________________________
+    /*1. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ].
+         С помощью цикла и условия заменить 0 на 1, 1 на 0;  */
     static void task1() {
         System.out.println("Задание 1");
         int[] task1 = {1, 0, 1, 0, 0, 1, 0};
@@ -35,6 +32,7 @@ public class Main {
         rollOver(task1);
         System.out.println(Arrays.toString(task1));
     }
+
     static void rollOver(int a[]){
         for (int locationNumber1 = 0; locationNumber1 < a.length; locationNumber1++) {
             if (a[locationNumber1] == 1) {
@@ -44,7 +42,9 @@ public class Main {
             }
         }
     }
+
     //second task_____________________________________________________
+    //2. Задать пустой целочисленный массив размером 8. С помощью цикла заполнить его значениями 0 3 6 9 12 15 18 21;
     static void task2(){
         System.out.println("Задание 2");
     int[] task2 = new int[8];
@@ -52,20 +52,25 @@ public class Main {
         multiplier(task2, locationNumber2);
         System.out.println(Arrays.toString(task2));
     }
+
     static void multiplier(int a[],int b){
         do {
             a [b] =b*3;
             b++;
         }while(b<a.length);
     }
+
     //third task______________________________________________________
+    //3. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2;
     static void task3(){
         System.out.println("Задание 3");
         int[] task3 ={1,5,3,2,11,4,5,2,4,8,9,1};
         int locationNumber3 = 0;
         smartMultiplier(task3,locationNumber3);
     }
+
     static void smartMultiplier(int a[],int b){
+
         while (b<=a.length) {
             if (a [b] <6){
                 while(a [b] <6){
@@ -73,34 +78,43 @@ public class Main {
                 }
                 b++;
             }
+
             else if (b<a.length){
                 b++;
             }
+
             if(b==a.length) {
                 System.out.println(Arrays.toString(a));
                 break;
             }
         }
     }
+
     //fourth task_____________________________________________________
+    //4. Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое), и с помощью
+    //цикла(-ов) заполнить его диагональные элементы единицами;
     static void task4(){
         System.out.println("Задание 4");
         int[][]task4 = new int [6][6];
         diagonalWithOnes(task4);
         printSquare(task4);
     }
+
     static void printSquare(int [][]square){
         for (int i=0;i<square.length;i++){
             System.out.println(Arrays.toString(square[i]));
         }
     }
+
     static void diagonalWithOnes(int [][]square){
         for(int i=0;i<square.length;i++){
             square[i][i]=1;
             square[i][square[1].length - 1-i]=1;
         }
     }
+
     //fifth task______________________________________________________
+    // 5. ** Задать одномерный массив и найти в нем минимальный и максимальный элементы (без помощи интернета);
     static void task5() {
         System.out.println("Задание 5");
         int[] task5 = new int[9];
@@ -110,6 +124,7 @@ public class Main {
         System.out.println(Arrays.toString(task5));//вывести первоначальные значения массива от 0 до 9
         minOrMax(task5);
     }
+
     static void minOrMax(int a[]){
         int max = 0;
         int min = 9;
@@ -118,6 +133,7 @@ public class Main {
                 min=a[x];
             }
         }
+
         for(int x = 0;x<a.length;x++){
             if (a[x]>max){
                 max=a[x];
@@ -126,16 +142,34 @@ public class Main {
         System.out.println("Минимальное значение = "+min+"\nМаксимальное значение = "+max);
 
     }
+
     static void randomArray(int arr[],int l){
         do {
             arr [l] =randomNumber();
             l++;
         }while(l<arr.length);
     }
+
     static int randomNumber(){
         Random random = new Random();
         int x = random.nextInt(9);
         return x;
     }
+//6. ** Написать метод, в который передается не пустой одномерный целочисленный массив, метод должен вернуть true,
+//если в массиве есть место, в котором сумма левой и правой части массива равны.
+//Примеры: checkBalance([2, 2, 2, 1, 2, 2, || 10, 1]) → true, checkBalance([1, 1, 1, || 2, 1]) → true, граница
+//показана символами ||, эти символы в массив не входят.
 
+//7. **** Написать метод, которому на вход подается одномерный массив и число n (может быть положительным, или
+//отрицательным), при этом метод должен сместить все элементымассива на n позиций. Для усложнения задачи нельзя
+//пользоваться вспомогательными массивами.
+
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+
+    }
 }
--- lesson2/src/ru/geekbrains/lesson2/lesson2.java
+++ lesson2/src/ru/geekbrains/lesson2/lesson2.java
@@ -1,86 +1,235 @@
 package ru.geekbrains.lesson2;
 
 import java.util.Arrays;
+//1. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью
+//цикла и условия заменить 0 на 1, 1 на 0;
+//2. Задать пустой целочисленный массив размером 8. С помощью цикла заполнить его значениями 0 3 6 9 12 15 18 21;
+//3. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2;
+//4. Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое), и с помощью
+// цикла(-ов) заполнить его диагональные элементы единицами;
+//5. ** Задать одномерный массив и найти в нем минимальный и максимальный элементы (без помощи интернета);
+//6. ** Написать метод, в который передается не пустой одномерный целочисленный массив, метод должен вернуть
+//true, если в массиве есть место, в котором сумма левой и правой части массива равны. Примеры: checkBalance
+//([2, 2, 2, 1, 2, 2, || 10, 1]) → true, checkBalance([1, 1, 1, || 2, 1]) → true, граница показана символами
+//||, эти символы в массив не входят.
+//7. **** Написать метод, которому на вход подается одномерный массив и число n (может быть положительным,
+//или отрицательным), при этом метод должен сместить все элементымассива на n позиций. Для усложнения задачи
+//нельзя пользоваться вспомогательными массивами.
 
 public class lesson2 {
 
+    private static void swap(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0){
+                arr[i] = 1;
+            }else{
+                arr[i]=0;
+            }
+        }
+    }
+
+    private static void fill(int[] arr){
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = i * 3;
+        }
+    }
+
+    private static void raise(int[] arr){
+        for (int i = 0; i <arr.length; i++){
+            if(arr[i] < 6){
+                arr[i] *= 2;                                                                                            // важно: умножить равно, по другому никак!
+            }
+        }
+    }
+
+    private static int findMin(int[] arr){
+        int min = arr[0];
+        for(int i = 0; i < arr.length; i++){
+            if(min > arr[i])
+                min = arr[i];
+        }
+        return min;
+    }
+
+    private static int findMax(int[] arr){
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++){
+            if(max < arr[i])
+                max = arr[i];
+        }
+        return max;
+    }
+
+    private static void diag(int[][] arr){
+        for (int i = 0; i < arr.length; i++){
+            arr[i][i] = 1;
+            arr[i][arr.length - 1 -i] = 1;
+        }
+    }
+
+    private static boolean checkBalance(int[] arr){
+        int left = 0;
+        for (int i = 0; i < arr.length - 1; i++){
+            left =+ arr[i];
+            int right = 0;
+            for (int j = i + 1; j < arr.length; j++){
+                right =+ arr[j];
+            }
+            if (left == right)
+                return true;
+        }
+        return false;
+    }
+
+    private static boolean checkBalance1(int[] arr){                                                                    //оптимизация метода checkBalance
+        int sum = 0;
+        for(int i = 0; i < arr.length; i++){
+            sum += arr[i];                                                                                              //важно сначала + потом =
+        }
+        if(sum % 2 != 0) return false;
+        int left = 0;
+        for(int i = 0; i < arr.length; i++){
+            left += arr[i];
+            sum -= arr[i];
+            if(sum == left)return true;
+       }
+        return false;
+    }
+
+    private static void shift(int[] arr, int n){                                                                        //*** костыльный вариант реализации 8 задания
+        boolean dir;
+        if(n < 0){                                                                                                      //*** определяем направление
+            dir = true;
+            n = -n;                                                                                                     //*** инвертируем значение направления. это нужно чтобы в цикле бежать
+                                                                                                                        // в положительную сторону
+        }
+        else{
+            dir = false;
+        }
+        n %= arr.length;                                                                                                //*** упращает сдвиги привышающие длинну массива, -> n=6 length=4 ==> n=2
+        int last = arr.length - 1;                                                                                      //*** просто переменная с значением arr.length - 1
+        for (int i = 0; i < n; i++) {                                                                                   //*** n теперь всегда положительная и создавать отдельный метод
+                                                                                                                        // для расчета отрицательных - не нужно
+            int temp = (dir) ? arr[0] : arr[last];                                                                      //*** вытаскивает в зависимости от dir первое или последнее значение
+                                                                                                                        // из массива
+            for (int j = 0; j < last; j++) {
+                if(dir){
+                    arr[j] = arr[j + 1];
+                }
+                else{
+                    arr[last - j] = arr[last - j - 1];
+                }
+            }
+           if (dir){
+               arr[last] = temp;
+           }
+           else{
+               arr[0] = temp;
+           }
+        }
+    }
+
+    private static void shift2(int[] arr, int n){                                                                       //выполняет в точности те же функции, в той же последовательности,
+                                                                                                                        // что и метод shift, но записан более красиво и компактно
+        boolean dir = n < 0;
+        if (dir)
+            n = - n;
+        n %= arr.length;
+        int last = arr.length - 1;
+        for (int i = 0; i < n; i++) {
+            int temp = (dir) ? arr[0] : arr[last];
+            if (dir){
+                System.arraycopy(arr, 1, arr, 0, last);
+                arr[last] = temp;
+            }
+            else {
+                System.arraycopy(arr, 0, arr, 1, last);
+                arr[0] = temp;
+            }
+        }
+    }
+
+    private static void shift3(int[] arr, int n){
+        boolean dir = n < 0;
+        if (dir)
+            n = - n;
+        n %= arr.length;
+
+        int shift = (dir) ? arr.length - n : n;
+        for (int i = 0; i < shift; i++) {
+            int temp = arr[arr.length - 1];
+            System.arraycopy(arr, 0, arr, 1, arr.length - 1);
+            arr[0] = temp;
+        }
+    }
+
     public static void main(String[] args) {
 
-     //first task________________________________________________________
-        System.out.println("Задание 1");
+        //1. Задать целочисленный массив, состоящий из элементов 0 и 1.
+        // Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ].
+        // С помощью цикла и условия заменить 0 на 1, 1 на 0;
+        int[] arr0 = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        System.out.println(Arrays.toString(arr0));
+        swap(arr0);
+        System.out.println(Arrays.toString(arr0));
+        System.out.println("task_1_____________________________________________________");
 
-        int[] task1 ={1,0,1,0,0,1,0};
+        //2. Задать пустой целочисленный массив размером 8.
+        // С помощью цикла заполнить его значениями 0 3 6 9 12 15 18 21
+        int[] arr1 = new int[8];
+        fill(arr1);
+        System.out.println(Arrays.toString(arr1));
+        System.out.println("task_2_____________________________________________________");
 
-        System.out.println(Arrays.toString(task1));
+        //3. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ]
+        // пройти по нему циклом, и числа меньшие 6 умножить на 2;
+        int [] arr2 = { 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 };
+        System.out.println(Arrays.toString(arr2));
+        raise(arr2);
+        System.out.println(Arrays.toString(arr2));
+        System.out.println("task_3_____________________________________________________");
 
-        for(int locationNumber1 = 0; locationNumber1 < task1.length; locationNumber1++) {
-            if(task1[locationNumber1]==1){
-                task1[locationNumber1]=0;
-            }
-            else{
-                task1[locationNumber1]=1;
-            }
+        //4. Создать квадратный двумерный целочисленный массив
+        // (количество строк и столбцов одинаковое), и с помощью
+        //цикла(-ов) заполнить его диагональные элементы единицами;
+        System.out.println(Arrays.toString(arr2));
+        System.out.println("min = " + findMin(arr2));
+        System.out.println("min = " + findMax(arr2));
+        System.out.println("task_4_____________________________________________________");
+
+        //5. ** Задать одномерный массив и найти в нем минимальный
+        //и максимальный элементы (без помощи интернета);
+        int SIZE = 10;
+        int[][] arr3 = new int[SIZE][SIZE];
+        diag(arr3);
+        for(int i = 0; i <SIZE; i++) {                                                                                  //реализация вывода двумерного массива
+            System.out.println(Arrays.toString(arr3[i]));
         }
-        System.out.println(Arrays.toString(task1));
+        System.out.println("task_5_____________________________________________________");
 
-     //second task_____________________________________________________
-        System.out.println("Задание 2");
+        //6. ** Написать метод, в который передается не пустой одномерный
+        // целочисленный массив, метод должен вернуть true, если в массиве
+        // есть место, в котором сумма левой и правой части массива равны.
+        // Примеры: checkBalance ([2, 2, 2, 1, 2, 2, || 10, 1]) → true,
+        // checkBalance([1, 1, 1, || 2, 1]) → true, граница показана символами
+        //||, эти символы в массив не входят.
+        int[] arr4 = {2, 2, 2, 1, 2, 2, 10, 1};
+        int[] arr5 = {1, 1, 1, 2, 1};
+        int[] arr6 = {1, 4, 3, 7, 8, 6, 1, 3, 5, 6, 1 ,6, 9, 2};
 
-        int[] task2 = new int[8];
-        int locationNumber2 = 0;
-        do {
-            task2 [locationNumber2] =locationNumber2*3;
-            locationNumber2++;
-        }while(locationNumber2<task2.length);
-        System.out.println(Arrays.toString(task2));
+        System.out.print(Arrays.toString(arr4));
+        System.out.println(" check balance is - " + checkBalance1(arr4));
+        System.out.print(Arrays.toString(arr5));
+        System.out.println(" check balance is - " + checkBalance1(arr5));
+        System.out.print(Arrays.toString(arr6));
+        System.out.println(" check balance is - " + checkBalance1(arr6));
+        System.out.println("task_6_____________________________________________________");
 
-        //third task_____________________________________________________
-       System.out.println("Задание 3");
+        //7. **** Написать метод, которому на вход подается одномерный массив и число n (может быть положительным,
+        //или отрицательным), при этом метод должен сместить все элементымассива на n позиций. Для усложнения задачи
+        //нельзя пользоваться вспомогательными массивами.
 
-        int[] task3 ={1,5,3,2,11,4,5,2,4,8,9,1};
-        int locationNumber3 = 0;
-
-        while (locationNumber3<=task3.length) {
-
-            if (task3 [locationNumber3] <6){
-                while(task3 [locationNumber3] <6){
-                    task3[locationNumber3] = task3[locationNumber3] * 2;
-                }
-                locationNumber3++;
-            }
-            else if (locationNumber3<task3.length){
-                locationNumber3++;
-            }
-            if(locationNumber3==task3.length) {
-                System.out.println(Arrays.toString(task3));
-                break;
-            }
-        }
-        //fourth task________________________________________________________
-        System.out.println("Задание 4");
-
-        int[][]task4 = new int [6][6];
-        //LocationNumber4
-
-        int x = 0;
-
-        for (int y = 0; y <task4.length; y++) {
-           if(y<task4.length){
-               while (x < task4.length) {
-                   if (x == y) {
-                       task4[x][y] = 1;
-                       x++;
-                   } else {
-                       task4[x][y] = 0;
-                       x++;
-                   }
-               }
-            }
-           else {
-               y++;
-           }
-
-        }
+        System.out.println("task_7_____________________________________________________");
 
     }
 }
